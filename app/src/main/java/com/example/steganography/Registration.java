83757858396e6df0331ab030fef84c787891c7d0
doaaessam98 <doaaessam2021@gmail.com>
2021-04-22 04:32:54 -0700

    splash screan with animation

--- app/src/main/java/com/example/steganography/Registration.java
+++ app/src/main/java/com/example/steganography/Registration.java
@@ -1,16 +1,14 @@
 package com.example.steganography;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    TextView t;
+public class Registration extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        t = findViewById(R.id.t);
+        setContentView(R.layout.activity_registration);
     }
 }
